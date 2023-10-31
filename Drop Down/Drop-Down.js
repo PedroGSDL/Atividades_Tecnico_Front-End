fd72ead9e08939860c960044fe63e8fc44964999
Pedro Gabriel Santos de Lima <116593548+PedroGSDL@users.noreply.github.com>
2023-10-31 16:23:03 -0300

    Add files via upload

---
+++ Drop Down/Drop-Down.js
@@ -0,0 +1,10 @@
+<script>
+  document.addEventListener('DOMContentLoaded', function () {
+    var icon = document.getElementById('icon');
+    var link = document.getElementById('link');
+
+    icon.addEventListener('click', function () {
+      link.click();
+    });
+  })
+</script>
